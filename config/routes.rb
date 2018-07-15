fb7a8434b0e726888e3f58a943235cd1e088bd56
Brian <riceb53@gmail.com>
2018-07-15 12:44:26 -0500

    add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,11 @@ Rails.application.routes.draw do
     get '/contacts/:id/edit' => 'contacts#edit'
     patch '/contacts/:id' => 'contacts#update'
     delete '/contacts/:id' => 'contacts#destroy'
+
+    get "/signup" => "users#new"
+    post "/users" => "users#create"
+    get "/login" => "sessions#new"
+    post "/login" => "sessions#create"
+    delete "/logout" => "sessions#destroy"
   end
 end
